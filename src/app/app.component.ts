386458788ae86d67e585e75493c645cfaf64d4ad
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-16 23:47:02 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,45 +7,40 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'inversenumber';
-  number:boolean=false;
-  nomode:boolean=false
-  fractional:boolean=false;
-  Developer:boolean=false;
-  output:string
-  ShowOutput:boolean=false;
-  ngOnInit(){
+  title = 'inversenumber'; number: boolean = false; nomode: boolean = false; fractional: boolean = false;
+  Developer: boolean = false;
+  output: string
+  ShowOutput: boolean = false;
+  ngOnInit() {
 
   }
-  settypenumber(){
-    this.number=true;
-    this.nomode=true;
-    this.ShowOutput=true
+  settypenumber() {
+    this.number = true; this.nomode = true;
+    this.ShowOutput = true
   }
-  settypeFraction(){
-    this.fractional=true
-    this.nomode=true;
-     this.ShowOutput=true
+  settypeFraction() {
+    this.fractional = true
+    this.nomode = true;
+    this.ShowOutput = true
   }
-  numbersubmit(f:NgForm)
-  {
-    this.output="1/"+f.value.num;
+  numbersubmit(f: NgForm) {
+    this.output = "1/" + f.value.num;
 
   }
-  fractionSubmit(f:NgForm){
-    this.output=`${f.value.num}/${f.value.dem}`
+  fractionSubmit(f: NgForm) {
+    this.output = `${f.value.num}/${f.value.dem}`
   }
-  changeMode(){
-    this.number=false;
-    this.fractional=false;
-    this.nomode=false;
-   this. output="";
-    this.ShowOutput=false;
-    this.Developer=false
+  changeMode() {
+    this.number = false;
+    this.fractional = false;
+    this.nomode = false;
+    this.output = "";
+    this.ShowOutput = false;
+    this.Developer = false
   }
-  developer(){
-    this.Developer=true;
-    this.nomode=true
+  developer() {
+    this.Developer = true;
+    this.nomode = true
 
   }
 }
