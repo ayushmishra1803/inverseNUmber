b404e58cd2104a46b3715b01a75956557bc6f646
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-16 20:44:44 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'inversenumber';
+  number:boolean=false;
+  nomode:boolean=false
+  fractional:boolean=false
+  output:string
+  ngOnInit(){
+
+  }
+  settypenumber(){
+    this.number=true;
+    this.nomode=true
+  }
+  settypeFraction(){
+    this.fractional=true
+    this.nomode=true
+  }
+  numbersubmit(f:NgForm)
+  {
+    this.output="1/"+f.value.num;
+
+  }
+  fractionSubmit(f:NgForm){
+    this.output=`${f.value.num}/${f.value.dem}`
+  }
+  changeMode(){
+    this.number=false;
+    this.fractional=false;
+    this.nomode=false;
+   this. output=""
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
